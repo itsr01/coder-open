867947f424c38f78e837d26e53dad97701056bae
itsr01 <rauldiasmendes94@gmail.com>
2020-08-10 12:31:00 -0400

    200810-01

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+function darkToggle() {
+    document.getElementById("bode").classList.toggle('bg-dark-blue');
+    document.getElementById("bode").classList.toggle('text-white');
+    document.getElementById("navtest").classList.toggle('navbar-dark');
+}
