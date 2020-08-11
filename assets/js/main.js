ca017df567f8fa3e4c6cfeb77dd9fd434f70275e
itsr01 <rauldiasmendes94@gmail.com>
2020-08-11 12:23:25 -0400

    200811-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,37 @@
+// function darkToggle() {
+//     document.getElementById("bode").classList.toggle('bg-dark-blue');
+//     document.getElementById("bode").classList.toggle('text-white');
+//     document.getElementById("mainNav").classList.toggle('navbar-dark');
+// }
+
+// ---------------------------------------------------------------------------------
+
+var darkThemeActive = JSON.parse(localStorage.getItem('dark_theme')) || [];
+
+if (darkThemeActive == true) {
+    darkToggleOn();
+    document.getElementById("toggleDarkMode").setAttribute('checked', 'checked');
+}
+
 function darkToggle() {
-    document.getElementById("bode").classList.toggle('bg-dark-blue');
-    document.getElementById("bode").classList.toggle('text-white');
-    document.getElementById("mainNav").classList.toggle('navbar-dark');
+    var darkThemeActive = JSON.parse(localStorage.getItem('dark_theme')) || [];
+    if (darkThemeActive == true) {
+        darkToggleOff();
+    } else {
+        darkToggleOn();
+    }
+}
+
+function darkToggleOn() {
+    document.getElementById("bode").classList.add('bg-dark-blue');
+    document.getElementById("bode").classList.add('text-white');
+    document.getElementById("mainNav").classList.add('navbar-dark');
+    localStorage.setItem('dark_theme', true);
+}
+
+function darkToggleOff() {
+    document.getElementById("bode").classList.remove('bg-dark-blue');
+    document.getElementById("bode").classList.remove('text-white');
+    document.getElementById("mainNav").classList.remove('navbar-dark');
+    localStorage.setItem('dark_theme', false);
 }
